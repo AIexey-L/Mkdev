325725f6ae438bee97886276a8d3bdbd12ae3c1d
Alex Lopatin <lopatin.rnd@gmail.com>
2016-10-18 06:04:46 +0300

    Added file handling, output

--- Movie/Movie_v1.rb
+++ Movie/Movie_v1.rb
@@ -1,7 +1,25 @@
 
+input = ARGV.join # make
+filename = "movies.txt"
+
+unless input == "" # in case of empty filename default file loads
+  filename = input
+end
+
+unless File.exist?(filename) # puts readable comment if file don't exist
+  puts "_"*100
+  puts "*"*100
+  puts " "*100
+  puts "THERE IS NO FILE NAMED: #{filename}, PLEASE, ENTER VALID FILENAME OR SKIP ENTERING AND START PROGRAMM."
+  puts "IF NO VALID FILENAME WILL BE ENTERED, PROGRAM WILL BE STARTED WITH DEFAULT FILE: movies.txt"
+  puts " "*100
+  puts "+"*100
+  puts "~"*100
+end
+
 
 content = []
-IO.foreach("movies.txt") do |line| # IO.foreach reads file per line, less memory usage in case of big fle
+IO.foreach(filename) do |line| # IO.foreach reads file per line, less memory usage in case of big fle
    content.push(line.chomp) # make a array of movies
 end
 
@@ -18,7 +36,7 @@ relevant_movies = []
 splited.each do |film|
   string = film.join(" ")
   if string.include? ("Time")
-    relevant_movies.push([film[1], star_rating(film[7].to_f)])
+    relevant_movies.push([film[1], star_rating(film[7].to_f)]) # make arrays with movie names and ratings
   end
 end
 
@@ -26,3 +44,7 @@ end
 puts format("%30s | %s", "Movie", "Rating")
 puts "-"*50
 relevant_movies.each { |movie| puts format("%30s | %s", "#{movie[0]}", "#{movie[1]}")}
+
+
+# File и String - это классы объектов Ryby. Имеюют встроенные методы.
+# Опредеоить коасс любого объекта в Ruby можно методом .class (puts <somthing>.class)
