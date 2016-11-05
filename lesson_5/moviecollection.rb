0a6fe4a2ab3b7150b40aeb8f242e5660a847106f
Alex Lopatin <lopatin.rnd@gmail.com>
2016-11-06 00:22:34 +0300

    Rewright adressing Movie varables from MovieCollection with .send

--- lesson_5/moviecollection.rb
+++ lesson_5/moviecollection.rb
@@ -14,9 +14,7 @@ class MovieCollection
     unless File.exist?(@filename)
       abort("\n\nTHERE IS NO FILE NAMED: #{filename}, PLEASE, ENTER VALID FILENAME.\nIF NO VALID FILENAME WILL BE ENTERED, PROGRAM WILL BE STARTED\n  WITH DEFAULT FILE: movies.txt\n\n")
     end
-
     @collection_array = CSV.foreach(@filename, col_sep: "|", headers: FIELDS).map { |x| Movie.new(x.to_h)}
-    hash_array = collection_array.map { |x| x.instance_variables.each_with_object({}) { |var, hash| hash[var.to_s.delete("@").to_sym] = x.instance_variable_get(var) }}
   end
 
   def all
@@ -24,13 +22,13 @@ class MovieCollection
   end
 
   def sort_by (for_sorting)
-    sorted = hash_array.sort_by { |x| x[for_sorting] }
+    sorted = @collection_array.sort_by { |x| x.send(for_sorting) }
   end
 
   def filter (field_hash)
-    n = hash_array
+    n = @collection_array
      field_hash.each do |k,v|
-       m = n.find_all { |x| x[k].include?("#{v}")}
+       m = n.find_all { |x| x.send(k).include?("#{v}")}
        n = m
      end
      n
@@ -38,11 +36,11 @@ class MovieCollection
 
   def stats(field)
     if field == :month
-      stats = hash_array.reject { |field| field.release_date.to_s[5..6] == nil }.map { |field| Date.strptime(field.release_date, "%Y-%m").strftime("%B") }.group_by { |x| x }
+      stats = @collection_array.reject { |field| field.release_date.to_s[5..6] == nil }.map { |field| Date.strptime(field.release_date, "%Y-%m").strftime("%B") }.group_by { |x| x }
     elsif field == :year
-      stats = hash_array.map { |field| Date.strptime(field.release_date, "%Y").strftime("%Y") }.group_by { |x| x }
+      stats = @collection_array.map { |field| Date.strptime(field.release_date, "%Y").strftime("%Y") }.group_by { |x| x }
     else
-      stats = hash_array.map { |x| x[field].to_s.split(",")}.flatten(1).group_by { |x| x}
+      stats = @collection_array.map { |x| x.send(field).to_s.split(",")}.flatten(1).group_by { |x| x}
     end
     stats.each { |k, v| stats[k] = v.count }.sort_by { |k, v| v }.to_h
   end
